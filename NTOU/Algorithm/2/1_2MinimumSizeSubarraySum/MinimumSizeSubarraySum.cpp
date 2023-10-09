9ed6fa9498a7db7a3bdaed5cb35a5267a28e9e70
GNYAR <s901121s901121@gmail.com>
2023-10-09 20:44:39 +0800

    feat(al): minimum size subarray sum

--- NTOU/Algorithm/2/1_2MinimumSizeSubarraySum/MinimumSizeSubarraySum.cpp
+++ NTOU/Algorithm/2/1_2MinimumSizeSubarraySum/MinimumSizeSubarraySum.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
-#define MAX_N 200
+#include <climits>
 
 using namespace std;
+
+int search(int *xs, int n, int x)
+{
+  int left = 0, right = n - 1;
+  while (left <= right)
+  {
+    int middle = (left + right) / 2;
+    if (xs[middle] == x)
+      return middle;
+    else if (xs[middle] > x)
+      right = middle - 1;
+    else
+      left = middle + 1;
+  }
+  return left >= n ? -1 : left;
+}
 
 int main()
 {
   int n, s;
   cin >> n >> s;
-  int *arr = new int[n];
+  int *arr = new int[n], *sums = new int[n + 1]{0}, ans = INT_MAX;
   for (int i = 0; i < n; i++)
+  {
     cin >> arr[i];
-
-  int x = MAX_N;
-  for (int i = 0; i < n; i++)
-  {
-    int sum = 0, index = i;
-    while (sum < s && index < n)
-      sum += arr[index++];
-    if (sum >= s)
-      x = min(x, index - i);
+    sums[i + 1] = sums[i] + arr[i];
   }
-  for (int i = n - 1; i > -1; i--)
+  for (int i = 1; i <= n; i++)
   {
-    int sum = 0, index = i;
-    while (sum < s && index > -1)
-      sum += arr[index--];
-    if (sum >= s)
-      x = min(x, i - index);
+    int x = s + sums[i - 1];
+    int res = search(sums, n + 1, x);
+    if (res > 0)
+      ans = min(ans, res - i + 1);
   }
-  cout << (x == MAX_N ? 0 : x) << endl;
+  cout << (ans == INT_MAX ? 0 : ans) << endl;
 
-  delete[] arr;
+  delete[] arr, sums;
   return 0;
 }
