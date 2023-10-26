2213fe471bf80251a0a452f78f6b4831494cd300
GNYAR <s901121s901121@gmail.com>
2023-10-27 00:28:16 +0800

    feat(ds): sum, product, difference, quotient, remainder

---
+++ NTOU/DataStructure/week1/q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  cout << a << '+' << b << '=' << a + b << endl;
+  cout << a << '*' << b << '=' << a * b << endl;
+  cout << a << '-' << b << '=' << a - b << endl;
+  cout << a << '/' << b << '=' << a / b;
+  if (a % b)
+    cout << "..." << a % b;
+  cout << endl;
+  return 0;
+}
