76ff21204c2a029b9887ecc997780b76846d3b93
GNYAR <s901121s901121@gmail.com>
2023-12-28 01:42:39 +0800

    refactor(ds): prim algorithm use vector instead of pair

--- NTOU/DataStructure/week15/q2.cpp
+++ NTOU/DataStructure/week15/q2.cpp
@@ -5,39 +5,45 @@ using namespace std;
 
 int main()
 {
-	vector<vector<pair<int, int>>> graph;
-	int s, x, y, z, n, cnt = 0;
-	cin >> s;
-	while (cin >> x >> y >> z)
+	vector<vector<vector<int>>> graph; // {id, weight}
+	int start, x, y, w;
+	cin >> start;
+	while (cin >> x >> y >> w)
 	{
 		if (max(x, y) >= graph.size())
 			graph.resize(max(x, y) + 1);
-		graph[x].push_back(make_pair(z, y));
-		graph[y].push_back(make_pair(z, x));
+		graph[x].push_back({y, w});
+		graph[y].push_back({x, w});
 	}
-	x = s;
-	s = graph.size();
-	n = 0;
-	vector<vector<bool>> flags(s, vector<bool>(s, true));
+
+	int n = graph.size(), cnt = 0, cost = 0;
+	// [i][j]: is edge(i, j) selected
+	vector<vector<bool>> flags(n, vector<bool>(n, false));
+	// min heap for edge {weight, v, u}
 	priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-	while (n < s - 1)
+	x = start;
+	while (cnt < n - 1) // edge count = n - 1
 	{
+		// push edge into min heap
 		for (int i = 0; i < graph[x].size(); i++)
 		{
-			y = graph[x][i].second;
-			z = graph[x][i].first;
-			if (flags[x][y])
+			y = graph[x][i][0];
+			w = graph[x][i][1];
+			if (!flags[x][y])
 			{
-				pq.push({z, x, y});
-				flags[x][y] = flags[y][x] = false;
+				pq.push({w, x, y});
+				flags[x][y] = flags[y][x] = true;
 			}
 		}
+
+		// select the minimum cost edge
 		vector<int> e = pq.top();
 		pq.pop();
-		cout << ++n << ": <" << e[1] << ',' << e[2] << ">\n";
-		cnt += e[0];
+		cost += e[0];
+		cout << ++cnt << ": <" << e[1] << ',' << e[2] << ">\n";
+
 		x = e[2];
 	}
-	cout << "\nThe cost of minimum spanning tree: " << cnt << '\n';
+	cout << "\nThe cost of minimum spanning tree: " << cost << '\n';
 	return 0;
 }
